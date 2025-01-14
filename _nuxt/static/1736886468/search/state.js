313cc08e35c95f32d8421a79d7e0b9db8dc69a90
danmillr <danmillr@users.noreply.github.com>
2025-01-14 20:42:37 +0000

    deploy: 9f8d3e900aab1298bce4bc50f0631f27cb8c6e0d

--- _nuxt/static/1736886468/search/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fplatform\u002F_nuxt\u002Fstatic\u002F1736886468",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsearch",config:{_app:{basePath:"\u002Fplatform\u002F",assetsPath:"\u002Fplatform\u002F_nuxt\u002F",cdnURL:a}}}}(null));
