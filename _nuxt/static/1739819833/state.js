617e4fa6c3db10d4323f532dd8b6fce89a9b27db
danmillr <danmillr@users.noreply.github.com>
2025-02-17 19:54:17 +0000

    deploy: 7624b66648da79d9abff96721b49451ad745ce3a

--- _nuxt/static/1739819833/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fplatform\u002F_nuxt\u002Fstatic\u002F1739819833",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fplatform\u002F",assetsPath:"\u002Fplatform\u002F_nuxt\u002F",cdnURL:a}}}}(null));
